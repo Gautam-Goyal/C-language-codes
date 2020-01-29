19f6e3b2a9cb07212af855777ab83a20e68690b5
Gautam-Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-01-30 00:02:20 +0530

    Update reverse a number.cpp

--- reverse a number.cpp
+++ reverse a number.cpp
@@ -4,27 +4,27 @@ main()
 {
 	int n1,n,q,r,rev=0;
 	printf("enter any three digit number ");
-	scanf("%d",n);
+	scanf("%d",&n);
 	n1=n;
 	q=n/10;
 	r=n%10;
-	//rev=rev*10+r;
-	printf("%d",r);
+	rev=rev*10+r;
+	//printf("%d",r);
 	n=q;
 	
 	q=n/10;
 	r=n%10;
-	//rev=rev*10+r;
-	printf("%d",r);
+	rev=rev*10+r;
+	//printf("%d",r);
 	n=q;
 	
 	q=n/10;
 	r=n%10;
-	//rev=rev*10+r;
-	printf("%d",r);
+	rev=rev*10+r;
+	//printf("%d",r);
 	n=q;
 	
-//	printf("therefore reverse of no. n %d is %d",n1,rev);
+	printf("therefore reverse of no. n %d is %d",n1,rev);
 	
 	getch();
 }
