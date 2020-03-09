37d2369a3d5ed4e09d53b0aa297d9ce58f94ffd2
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-03-09 17:08:42 +0530

    Pointer to pointer

---
+++ pointer to pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i=3,*j,**k;
+	j=&i;
+	k=&j;
+	printf("Address of i=%u\n",&i);
+    printf("Address of i=%u\n",j);
+	printf("Address of i=%u\n",*k);
+	printf("Address of j=%u\n",&j);
+	printf("Address of j=%u\n",k);
+	printf("Address of k=%u\n",&k);
+	printf("Value of j=%u\n",j);
+	printf("Value of k=%u\n",k);
+	printf("Value of i=%u\n",i);
+	printf("Value of i=%u\n",*j);
+	printf("Value of i=%u\n",*(&i));	
+	printf("Value of i=%u\n",**k);
+	return 0;		
+}
